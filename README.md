79c7c18a33e0154e92c75385a3105eed6f206093
Vince <89287198+vcaristo@users.noreply.github.com>
2025-04-18 17:36:11 -0600

    add single-node conda, apptainer examples

---
+++ README.md
@@ -0,0 +1,9 @@
+# Hellgate Demos
+
+This is a repository for demonstrations of code that runs on the University of Montana's Hellgate HPC cluster.  
+
+The intention is to provide working examples that others can modify for their own uses. Use cases include:
+- 1-node SLURM jobs with conda environments
+- 1-node SLURM jobs with apptainer containers
+- Multi-node SLURM jobs (parallelize across nodes)
+- <hopefully more coming soon!>
---
+++ conda_examples/osm_download_1-node/osm_download_batch.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --job-name=osm-download
+#SBATCH --output=osm-download.out
+#SBATCH --error=osm-download.err
+#SBATCH --time=12:00:00
+#SBATCH --mem=16G
+#SBATCH --cpus-per-task=1
+#SBATCH --mail-type=all
+#SBATCH --mail-user=vc149353@umconnect.umt.edu
+
+# Load conda and activate environment
+module load conda
+
+# init conda
+source $(conda info --base)/etc/profile.d/conda.sh
+
+conda activate us_osm_env
+
+# Run the script
+python download_osm_features.py
